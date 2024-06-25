093585b459db8e6a841ff074f5536aef2117117e
Vsomera <vsomera77@outlook.com>
2024-06-24 21:36:24 -0700

    refactor + routing

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"http-server/storage"
 	"log"
 	"net/http"
 	"strings"
@@ -17,18 +18,33 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	router := http.NewServeMux()
+
+	router.Handle("/league", http.HandlerFunc(p.LeagueHandler))
+
+	router.Handle("/players/", http.HandlerFunc(p.PlayerHandler))
+
+	router.ServeHTTP(w, r)
+}
+
+func (p *PlayerServer) LeagueHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func (p *PlayerServer) PlayerHandler(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
 	switch r.Method {
 	case http.MethodPost:
-		p.processWin(w, r)
+		p.processWin(w, player)
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, player)
 	}
 }
 
 // GET
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
@@ -39,16 +55,13 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 }
 
 // POST
-func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
 	p.store.RecordWin(player)
-
 	w.WriteHeader(http.StatusAccepted)
 }
 
 func main() {
-	store := NewInMemoryPlayerStore()
+	store := storage.NewInMemoryPlayerStore()
 	server := &PlayerServer{store}
 
 	log.Fatal(http.ListenAndServe(":5000", server))
